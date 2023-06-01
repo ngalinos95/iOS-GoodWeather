946accdeca8ced3a0edebf45b8319555c361b2e1
ngalinos95 <ngalinos95@me.com>
2023-06-01 12:57:15 +0300

    Initial Commit

---
+++ GoodWeather/GoodWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherApp.swift
+//  GoodWeather
+//
+//  Created by Nikos Galinos on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
